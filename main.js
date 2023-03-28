aabeb876dc8a3284b64632a0c2b5016c9d930256
Melissa Villegas <melissavillegas0898@gmail.com>
2023-03-28 18:53:39 -0400

    js for rating + checked and hidden classes added

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+const ratings = document.querySelectorAll(".rating");
 const btn = document.querySelector(".submit");
+const selectedRating = document.querySelector("#selectedRating");
 const ratingState = document.querySelector(".rating-state");
 const thankYouState = document.querySelector(".thankYou-state");
+
+ratings.forEach(rating => {
+  rating.addEventListener("click", function () {
+    const active = document.querySelector(".checked");
+    if (active) {
+      active.classList.remove("checked");
+    }
+    rating.classList.add("checked");
+    selectedRating.innerText = rating.innerText;
+  });
+});
 
 function submitRating() {
   ratingState.classList.add("hidden");
